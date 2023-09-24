e20816cc4c4600ce9ab16824b5217056f7b16935
abn5365 <143108545+abn5365@users.noreply.github.com>
2023-09-24 13:00:35 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h1>This is a penguin</h1>
+        <img src="https://toppng.com/uploads/preview/club-penguin-old-penguin-11550188540ypyeuqw1as.png" alt="Frog" />
+        <div className="paragraph">
+          <p>This penguin is in a really cool club.</p>
+        </div>
+        <div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button> </a> </div>
+      </div>
+    );
+  }
+
+export default App;
